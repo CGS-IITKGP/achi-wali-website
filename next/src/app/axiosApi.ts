1897b52314a52c85d7fb1c8b345415fbf728f4aa
Neelabh Priyam Jha <183183932+athena-seguace@users.noreply.github.com>
2025-11-01 01:29:50 +0530

    fix(integration): fix api base url

--- next/src/app/axiosApi.ts
+++ next/src/app/axiosApi.ts
@@ -1,7 +1,7 @@
 import { IResponse } from "./types/response.types";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api/";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
 
 const axiosInstance = axios.create({
     validateStatus: () => true,
